f35ee7a5231181751f3b75ab275783c6e29175fb
Seb Gillman <59866963+SebGillman@users.noreply.github.com>
2022-05-31 16:03:14 +0100

    Added constant recording mode

--- Voiceprint_model/voiceprint-regonition/utils/record.py
+++ Voiceprint_model/voiceprint-regonition/utils/record.py
@@ -41,3 +41,26 @@ class RecordAudio:
         wf.writeframes(b''.join(frames))
         wf.close()
         return output_path
+
+    def recordconst(self, output_path="audio/temp.wav", record_seconds=3):
+        """
+        Terms meaning
+        :param output_path: path of save recording, with file format wav
+        :param record_seconds: record time, default setting will be 3s
+        :return: file path of the audio recordings
+        """
+        print("Recording......")
+        frames = []
+        for i in range(0, int(self.rate / self.chunk * record_seconds)):
+            data = self.stream.read(self.chunk)
+            frames.append(data)
+
+        print("Record finished!")
+        wf = wave.open(output_path, 'wb')
+        wf.setnchannels(self.channels)
+        wf.setsampwidth(self.p.get_sample_size(self.format))
+        wf.setframerate(self.rate)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+        return output_path
+
