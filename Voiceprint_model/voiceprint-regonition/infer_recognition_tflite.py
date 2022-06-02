79efdafc8a08fb41bb01906d083b462ba65ae539
horsada <horsada@gmail.com>
2022-06-02 16:23:04 +0100

    i/o updates

--- Voiceprint_model/voiceprint-regonition/infer_recognition_tflite.py
+++ Voiceprint_model/voiceprint-regonition/infer_recognition_tflite.py
@@ -97,7 +97,8 @@ if __name__ == '__main__':
     
     try:
         while True:
-            select_fun = int(input("Please type in number to choose function: type in 0 to register new member, type in 1 to do voice recognition, else type in 2 to do continuous recognition.\n"))
+            print('\n------------------------------------------------------------------')
+	    select_fun = int(input("Please type in number to choose function:\n type in 0 to register new member,\n type in 1 to do voice recognition,\n type in 2 to do continuous recognition.\n"))
             if select_fun == 0:
                 audio_path = record_audio.record()
                 name = input("Please type in your name as new member: ")
