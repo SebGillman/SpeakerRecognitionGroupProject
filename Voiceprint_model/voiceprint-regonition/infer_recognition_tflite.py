184bf7990cbf904970f22035a575a44bb4bab0b1
horsada <horsada@gmail.com>
2022-06-02 15:11:22 +0100

    stdout changes

--- Voiceprint_model/voiceprint-regonition/infer_recognition_tflite.py
+++ Voiceprint_model/voiceprint-regonition/infer_recognition_tflite.py
@@ -97,10 +97,10 @@ if __name__ == '__main__':
     
     try:
         while True:
-            select_fun = int(input("Please type in number to choose function: type in 0 to register new member, type in 1 to do voice recognition, else type in 2 to do continuous recognition."))
+            select_fun = int(input("Please type in number to choose function: type in 0 to register new member, type in 1 to do voice recognition, else type in 2 to do continuous recognition.\n"))
             if select_fun == 0:
                 audio_path = record_audio.record()
-                name = input("Please type in your name as new member ")
+                name = input("Please type in your name as new member: ")
                 if name == '': continue
                 register(audio_path, name)
             elif select_fun == 1:
@@ -110,9 +110,9 @@ if __name__ == '__main__':
                 time2 = time.time()
                 print('Classification time = ', np.round(time2-time1, 3), ' seconds.')
                 if p > args.threshold:
-                    print("The one currently speaking is %s with a similarity of %f" % (name, p))
+                    print("The one currently speaking is %s with a similarity of %f \n" % (name, p))
                 else:
-                    print("There's no matched member in the database,try speaking in your natural tone or avoid noisy enviroment")
+                    print("There's no matched member in the database,try speaking in your natural tone or avoid noisy enviroment \n")
             elif select_fun == 2:
                 print("Recording has started, press Ctrl+C to quit")
                 keypress=False
@@ -124,14 +124,14 @@ if __name__ == '__main__':
                         time2 = time.time()
                         print('Classification time = ', np.round(time2-time1, 3), ' seconds.')
                         if p > args.threshold:
-                            print("The one currently speaking is %s with a similarity of %f" % (name, p))
+                            print("The one currently speaking is %s with a similarity of %f \n" % (name, p))
                         else:
-                            print("There's no matched member in the database,try speaking in your natural tone or avoid noisy enviroment")
+                            print("There's no matched member in the database,try speaking in your natural tone or avoid noisy enviroment \n")
                 except KeyboardInterrupt:
                     pass
 
             else:
-                print('Please type either 0, 1 or 2')
+                print('Please type either 0, 1 or 2 \n')
                 
     except KeyboardInterrupt:
         pass
