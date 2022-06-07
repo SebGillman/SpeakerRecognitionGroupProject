4c4110a52a94ce5b514d14f13e3b5f9699487bf0
rccrd27 <58397596+rccrd27@users.noreply.github.com>
2022-06-07 19:19:08 +0100

    download trial5

--- src/AWS/s3_download_file.py
+++ src/AWS/s3_download_file.py
@@ -2,13 +2,12 @@ import boto3
 import os
 
 def download_files(bucket, local='./tmp'):
-
     # s3=boto3.client('s3')
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(bucket) 
+    os.makedirs(local)
 
     for obj in bucket.objects.all():
-        os.makedirs(os.path.dirname(local))
         bucket.download_file(obj.key, obj.key) # save to same path
 
 
