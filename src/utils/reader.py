b937806e36ae037623abfb046adc10388ba42d85
horsada <horsada@gmail.com>
2022-06-10 11:33:32 +0100

    Fixed librosa.display.specshow

--- src/utils/reader.py
+++ src/utils/reader.py
@@ -26,7 +26,7 @@ def load_audio(audio_path, mode='train', win_length=400, sr=44100, hop_length=16
 
     if stft_cloud:
         plt.figure()
-        librosa.display.specshow(mag, sr=sr, hop_length=hop_length, n_fft=n_fft, win_length=win_length, y_axis='log', x_axis='time')
+        librosa.display.specshow(mag, sr=sr, hop_length=hop_length, y_axis='log', x_axis='time')
         if name is not None:
             file_name = name+'.png'
         else:
