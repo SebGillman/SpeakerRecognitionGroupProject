8ecc8e91adf968ea73983d87fb3a869fd90b605c
rccrd27 <58397596+rccrd27@users.noreply.github.com>
2022-06-25 12:41:36 +0100

    sdc

--- src/utils/reader.py
+++ src/utils/reader.py
@@ -22,7 +22,7 @@ def load_audio(audio_path, mode='train', win_length=400, sr=16000, hop_length=16
         extended_wav = np.append(wav, wav[::-1])
     # calculate STFT
     #linear = librosa.stft(extended_wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length)
-    mel = librosa.feature.melspectrogram(extended_wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length)
+    mel = librosa.feature.melspectrogram(extended_wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length, n_mels=40)
     mag, _ = librosa.magphase(mel)
 
     
