bbef70409cb874f7d0065c6a6b22b58370d28053
rccrd27 <58397596+rccrd27@users.noreply.github.com>
2022-06-25 12:46:45 +0100

    ds

--- src/utils/reader.py
+++ src/utils/reader.py
@@ -11,7 +11,7 @@ import logging
 from botocore.exceptions import ClientError
 
 # Load and pre-process audio file
-def load_audio(audio_path, mode='train', win_length=400, sr=16000, hop_length=160, n_fft=512, spec_len=127, object_name=None, stft_cloud=False, name=None):
+def load_audio(audio_path, mode='train', win_length=400, sr=16000, hop_length=160, n_fft=512, spec_len=257, object_name=None, stft_cloud=False, name=None):
     # Load audio
     wav, sr_ret = librosa.load(audio_path, sr=sr)
     if mode == 'train':
@@ -21,9 +21,9 @@ def load_audio(audio_path, mode='train', win_length=400, sr=16000, hop_length=16
     else:
         extended_wav = np.append(wav, wav[::-1])
     # calculate STFT
-    #linear = librosa.stft(extended_wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length)
-    mag = librosa.feature.melspectrogram(extended_wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length, n_mels=128)
-    #mag, _ = librosa.magphase(mel)
+    linear = librosa.stft(extended_wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length)
+    #mel = librosa.feature.melspectrogram(extended_wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length, n_mels=128)
+    mag, _ = librosa.magphase(mel)
 
     
     # save the STFT in folder speactrograms
