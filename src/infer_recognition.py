3d986556439a22d21bf793ac29dd90652430ab3c
rccrd27 <58397596+rccrd27@users.noreply.github.com>
2022-06-29 12:02:10 +0100

    dsv

--- src/infer_recognition.py
+++ src/infer_recognition.py
@@ -20,7 +20,7 @@ parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
 add_arg('audio_db',         str,    'audio_db',               'path to our audio database')
 add_arg('input_shape',      str,    '(257, 257, 1)',          'shape of input data')
-add_arg('threshold',        float,   0.75,                     'threshold of verification')
+add_arg('threshold',        float,   0.8,                     'threshold of verification')
 add_arg('model_path',       str,    'models/infer_model.h5',  'path to model')
 args = parser.parse_args()
 
@@ -145,6 +145,7 @@ if __name__ == '__main__':
             select_fun = input("Please type in number to choose function:\n type in 0 to register new member,\n type in 1 to do single speaker recognition,\n type in 2 to do continuous speaker recognition, \n type in 3 to exit the program. \n")
 
             if select_fun == '0':
+                print("\nPlease speak slowly for a total of 3 seconds")
                 audio_path = record_audio.record()
                 name = input("Please type in your name as new member: ")
                 if name == '': continue
